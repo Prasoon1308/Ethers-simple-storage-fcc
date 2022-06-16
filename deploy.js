5d17453399041ea812ad46c51a93d8f5e392a0c4
Prasoon1308 <73938320+Prasoon1308@users.noreply.github.com>
2022-06-16 18:28:17 +0530

    Create deploy.js

---
+++ deploy.js
@@ -0,0 +1,10 @@
+//synchronous [solidity]
+//asynchronous [javascript]
+
+async function main() {
+  console.log("hi");
+  let variable = 5;
+  console.log(variable);
+}
+// deploy a contract? Wait for it to be deployed when async is used
+main();
